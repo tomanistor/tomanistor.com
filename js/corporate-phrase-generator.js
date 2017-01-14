350061fdca05e7c990ac9f5f345aec6fc062d3c5
Toma Nistor <Toma Nistor>
2017-01-13 20:18:42 -0800

    Added new phrases to Corporate Phrase Generator and added logos and favicons to portfolio

--- js/corporate-phrase-generator.js
+++ js/corporate-phrase-generator.js
@@ -1,15 +1,15 @@
 var phrases = [
-  'All it takes is a semi colon.',
+  'All it takes is a semicolon.',
   'Well, let\'s throw it through a for loop and see what it spits out.',
-  'Gotta tie your shoes before you go out for a run.',
+  'Tie your shoes before you go out for a run.',
   'Will it run on rails?',
   'Let\'s try to have an object oriented meeting.',
   'Don\'t be so object-oriented.',
   'I\'m working on dialup today.',
-  'Does bullshit stick?',
+  'Does it require a rake?',
   'Gotta run this through Capybara.',
   'Sounds like a breakroom problem.',
-  'Gotta reset the microwave to 0.',
+  'Don\'t forget to reset the microwave to 0.',
   'Sorry, my pen ran out of ink.',
   'Marker ran dry on that one.',
   'Are the lights coming on or am I going to be in the dark on this one?',
@@ -25,7 +25,16 @@ var phrases = [
   'Let\'s park this discussion.',
   'We\'re in violent agreement.',
   'We need to have a prioritization meeting.',
-  'Just because you put syrup on it doesn\'t make it pancakes'
+  'Just because you put syrup on it doesn\'t make it pancakes.',
+  'If you hang out in a barbershop long enough, you\'re bound to get your hair cut.',
+  'Don\'t try to put 20lbs of potatoes in a 10lb sack.',
+  'Are you going to walk me down this parade or what?',
+  'Be a comma, not a period.',
+  'Put a paperclip on it.',
+  'Can I take a look at your CSS?',
+  'Like making cheesecake from scratch.',
+  'Happy pre-hump day!',
+  'You know, Thursday is really just Wednesday\'s Friday.'
 ]
 
 // Getting random number in array
