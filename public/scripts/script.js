9bba06b1c9b837f756b49e7857b9c7f71d742c67
tomanistor <toma.nistor@gmail.com>
2017-03-23 01:31:45 -0700

    Added basic flexbox image grid

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 
+    // Nav starts at bottom then is fixed to top
     $(window).scroll(function() {
-        if( $(this).scrollTop() > 450 ) {
-            $(".nav").addClass("nav-scrolled");
+        if( $(this).scrollTop() > $(this).height() - $("nav").height() ) {
+            $("nav").addClass("nav-fixed");
         } else {
-            $(".nav").removeClass("nav-scrolled");
+            $("nav").removeClass("nav-fixed");
         }
     });
 
