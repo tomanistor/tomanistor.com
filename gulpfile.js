d9ad57a909b9916d4062d2560ec2092bfdaf742f
tomanistor <toma.nistor@gmail.com>
2017-06-17 23:52:06 -0700

    Static assets cache busting

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,60 @@
+// TODO: Figure out how to use pump the same way as .pipe so that hash maps manifested in data folder are not duplicated in data/scripts (css is present also)
+
 var gulp         = require("gulp"),
     sass         = require("gulp-sass"),
     autoprefixer = require("gulp-autoprefixer"),
     uglify       = require("gulp-uglify"),
+    hash         = require("gulp-hash"),
+    del          = require("del"),
     pump         = require("pump");
 
 // Minify JS
-gulp.task("compress", function (cb) {
-    pump([
-            gulp.src("themes/osprey/static/scripts/src/*.js"),
-            uglify(),
-            gulp.dest("themes/osprey/static/scripts")
-        ],
-        cb
-    );
+gulp.task("js", function (cb) {
+
+  // Delete old JS files
+  del(["themes/osprey/static/scripts/*.js"])
+
+  // Minifiy and hash JS files
+  pump([
+        gulp.src("themes/osprey/static/scripts/src/*.js"),
+        uglify(),
+        hash(),
+        gulp.dest("themes/osprey/static/scripts"),
+        // Create a hash map
+        hash.manifest("hash.json"),
+        // Put hash map in data folder
+        gulp.dest("data/scripts")
+      ],
+      cb
+  );
 });
 
-// Compile and Minify SCSS files to CSS
+// Compile and minify SCSS files to CSS
 gulp.task("scss", function () {
-    gulp.src("themes/osprey/static/styles/scss/main.scss")
-        .pipe(sass({
-            outputStyle : "compressed"
-        }))
-        .pipe(autoprefixer({
-            browsers : ["last 20 versions"]
-        }))
-        .pipe(gulp.dest("themes/osprey/static/styles"))
+
+  // Delete old CSS files
+  del(["themes/osprey/static/styles/*.css"])
+
+  // Compile and hash CSS files
+  gulp.src("themes/osprey/static/styles/scss/main.scss")
+      .pipe(sass({
+        outputStyle : "compressed"
+      }))
+      .pipe(autoprefixer({
+        browsers : ["last 20 versions"]
+      }))
+      .pipe(hash())
+      .pipe(gulp.dest("themes/osprey/static/styles"))
+      // Create a hash map
+      .pipe(hash.manifest("hash.json"))
+      // Put hash map in data folder
+      .pipe(gulp.dest("data/styles"))
 });
 
 // Watch scripts and styles folders for changes
-gulp.task("watch", ["compress", "scss"], function () {
-    gulp.watch("themes/osprey/static/scripts/src/*.js", ["compress"]),
-    gulp.watch("themes/osprey/static/styles/scss/main.scss", ["scss"])
+gulp.task("watch", ["js", "scss"], function () {
+  gulp.watch("themes/osprey/static/scripts/src/*.js", ["js"]),
+  gulp.watch("themes/osprey/static/styles/scss/*.scss", ["scss"])
 });
 
 // Set watch as default task
