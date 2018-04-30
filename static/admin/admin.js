48802cc23a0a2e1bcd3d80c8cb5ec69b62ccdb8b
Toma Nistor <toma.nistor@gmail.com>
2018-04-29 23:55:26 -0700

    Compiled admin.js

--- static/admin/admin.js
+++ static/admin/admin.js
@@ -1,4 +1,9 @@
-import CMS from 'netlify-cms'
+'use strict';
+
+var _netlifyCms = require('netlify-cms');
+var _netlifyCms2 = _interopRequireDefault(_netlifyCms);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 // Registry is available via the CMS object
-CMS.registerPreviewTemplate('cms-template', CmsTemplate)
+_netlifyCms2.default.registerPreviewTemplate('cms-template', CmsTemplate);
